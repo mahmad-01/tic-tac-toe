0e9d0a397dbb7ec1edfa2723e06c52a911653be5
mahmad-01 <mahmadbcs@gmail.com>
2023-06-12 10:36:09 -0300

    Board Created and Basic JavaScript Completed

---
+++ script.js
@@ -0,0 +1,11 @@
+const GameBoard = (() => {
+    const Player = (name, marker, isTurn) => {
+        return {name, marker, isTurn};
+    }
+    const Player1 = Player("Player 1", "X", true);
+    const Player2 = Player("PLayer 2", "O", false);
+    let gameB = [];
+
+
+
+})();
